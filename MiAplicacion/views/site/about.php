fada1eacff8866f998900c8019c50ac836e99d17
jonathanibarra <itt.13401097@gmail.com>
2015-12-09 19:32:59 -0700

    Edicion 21

--- MiAplicacion/views/site/about.php
+++ MiAplicacion/views/site/about.php
@@ -14,6 +14,19 @@ $this->params['breadcrumbs'][] = $this->title;
         Este Administrador de Tareas fue creado para la materia de Programación Web.
 
     </p>
-
-    <code><?= __FILE__ ?></code>
+        <p>
+        Los integrantes del equipo son:
+   		<ul>
+   		<li>Tejeda Mora Cesar</li>
+   		<li>Carrillo González Carlos Arturo</li>
+   		<li>Ibarra Maravillas Jonathan Guadalupe</li>
+   		<li>Ocampo Aguilar Jesus Andres</li>
+   		</ul>
+    </p>
+<center>
+    <p>
+        Instituto Tecnológico de Tepic
+    </p>
+<p> Ingeniería en Tecnologías de la Información y Comunicación
+    </center>
 </div>
